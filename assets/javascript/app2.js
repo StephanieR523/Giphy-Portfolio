c42596bfe1b411e1f22f96c704b095bc2ef9e8bb
Stephanie Romanowski <stephanie.romanowski@vantage-cg.com>
2019-07-09 19:19:36 -0400

    assets

---
+++ assets/javascript/app2.js
@@ -0,0 +1,11 @@
+{
+	"folders": [
+		{
+			"path": "C:\\Users\\StephanieRomanowski\\Desktop\\MyFolder"
+		},
+		{
+			"path": "C:\\Users\\StephanieRomanowski\\Desktop\\RUTSOM201905FSF3"
+		}
+	],
+	"settings": {}
+}
